261a911db1a048019ed46ba89923f526edc6eb29
ItsTonyFuckingStark <30393912+daviosardinha@users.noreply.github.com>
2026-08-26 18:12:41 +0100

    feat(theme): deploy Obsidian v2 and native Vim workflow

--- kalipwm.sh
+++ kalipwm.sh
@@ -11,25 +11,23 @@ if [ "$UID" -eq 0 ]; then
     echo -e "${RED}[-] No se puede ejecutar como root.${RESET}"
     exit 1
 else
-    # Comprobar si se está usando sudo
     if [ -n "$SUDO_USER" ]; then
         echo -e "${RED}[-] No uses sudo.${RESET}"
         exit 1
     fi
 fi
 
-# Banner en verde
 echo -e "${GREEN}
-@@@  @@@   @@@@@@   @@@       @@@  @@@@@@@   @@@  @@@  @@@  @@@@@@@@@@   
-@@@  @@@  @@@@@@@@  @@@       @@@  @@@@@@@@  @@@  @@@  @@@  @@@@@@@@@@@  
-@@!  !@@  @@!  @@@  @@!       @@!  @@!  @@@  @@!  @@!  @@!  @@! @@! @@!  
-!@!  @!!  !@!  @!@  !@!       !@!  !@!  @!@  !@!  !@!  !@!  !@! !@! !@!  
-@!@@!@!   @!@!@!@!  @!!       !!@  @!@@!@!   @!!  !!@  @!@  @!! !!@ @!@  
-!!@!!!    !!!@!!!!  !!!       !!!  !!@!!!    !@!  !!!  !@!  !@!   ! !@!  
-!!: :!!   !!:  !!!  !!:       !!:  !!:       !!:  !!:  !!:  !!:     !!:  
-:!:  !:!  :!:  !:!   :!:      :!:  :!:       :!:  :!:  :!:  :!:     :!:  
- ::  :::  ::   :::   :: ::::   ::   ::        :::: :: :::   :::     ::   
- :   :::   :   : :  : :: : :  :     :          :: :  : :     :      :    
+@@@  @@@   @@@@@@   @@@       @@@  @@@@@@@   @@@  @@@  @@@  @@@@@@@@@@
+@@@  @@@  @@@@@@@@  @@@       @@@  @@@@@@@@  @@@  @@@  @@@  @@@@@@@@@@@
+@@!  !@@  @@!  @@@  @@!       @@!  @@!  @@@  @@!  @@!  @@!  @@! @@! @@!
+!@!  @!!  !@!  @!@  !@!       !@!  !@!  @!@  !@!  !@!  !@!  !@! !@! !@!
+@!@@!@!   @!@!@!@!  @!!       !!@  @!@@!@!   @!!  !!@  @!@  @!! !!@ @!@
+!!@!!!    !!!@!!!!  !!!       !!!  !!@!!!    !@!  !!!  !@!  !@!   ! !@!
+!!: :!!   !!:  !!!  !!:       !!:  !!:       !!:  !!:  !!:  !!:     !!:
+:!:  !:!  :!:  !:!   :!:      :!:  :!:       :!:  :!:  :!:  :!:     :!:
+ ::  :::  ::   :::   :: ::::   ::   ::        :::: :: :::   :::     ::
+ :   :::   :   : :  : :: : :  :     :          :: :  : :     :      :
 "
 
 sleep 2
@@ -42,36 +40,30 @@ sleep 3
 
 RPATH=`pwd`
 
-# Actualizar paquetes
 echo -e "\n${BLUE}[*] Actualizando paquetes..${RESET}\n"
 sudo apt update
 
-# Instalar paquetes
 echo -e "\n${BLUE}[*] Instalando paquetes..${RESET}\n"
 sudo apt install -y git bspwm vim feh scrot scrub zsh rofi xclip xsel locate wmname acpi sxhkd \
     imagemagick ranger kitty tmux python3-pip font-manager lsd bat bpython open-vm-tools-desktop open-vm-tools fastfetch \
     dirsearch feroxbuster gedit curl wget unzip papirus-icon-theme lm-sensors pavucontrol network-manager i3lock jq
 
-# Instalar dependencias del entorno
 echo -e "\n${BLUE}[*] Instalando dependencias del entorno..${RESET}\n"
 sudo apt install -y build-essential libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev \
     libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev
 
-# Instalar requisitos de polybar
 echo -e "\n${BLUE}[*] Instalando requisitos de polybar..${RESET}\n"
 sudo apt install -y cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev \
     libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev \
     libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev \
     libmpdclient-dev libuv1-dev libnl-genl-3-dev
 
-# Instalar dependencias de picom
 echo -e "\n${BLUE}[*] Instalando dependencias de picom..${RESET}\n"
 sudo apt install -y meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev \
     libxcb-render-util0-dev libxcb-render0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev \
     libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev \
     uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev libpcre3 libpcre3-dev
 
-# Instalar Hack Nerd Font
 echo -e "\n${BLUE}[*] Instalando fuentes..${RESET}\n"
 mkdir -p /tmp/fonts
 wget -q --show-progress https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip -O /tmp/fonts/Hack.zip
@@ -81,7 +73,6 @@ mv /tmp/fonts/*.ttf ~/.local/share/fonts/
 rm -rf /tmp/fonts
 fc-cache -fv
 
-# Instalar JetBrains Mono Nerd Font
 mkdir -p /tmp/fonts
 wget -q --show-progress https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip -O /tmp/fonts/JetBrainsMono.zip
 unzip -q /tmp/fonts/JetBrainsMono.zip -d /tmp/fonts
@@ -89,52 +80,36 @@ mv /tmp/fonts/*.ttf ~/.local/share/fonts/
 rm -rf /tmp/fonts
 fc-cache -fv
 
-# Instalar ohmyzsh
 echo -e "\n${BLUE}[*] Instalando OhMyZSH..${RESET}\n"
 rm -rf ~/.oh-my-zsh
 yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Instalar powerlevel10k
 echo -e "\n${BLUE}[*] Instalando tema powerlevel10k..${RESET}\n"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 rm -f ~/.p10k.zsh
 cp -v $RPATH/CONFIGS/p10k.zsh ~/.p10k.zsh
 
-# Instalar plugins de zsh
 echo -e "\n${BLUE}[*] Instalando plugins de ZSH..${RESET}\n"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 rm -f ~/.zshrc
 cp -v $RPATH/CONFIGS/zshrc ~/.zshrc
 
-# Instalar fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 yes | ~/.fzf/install
 
-# .tmux
-echo -e "\n${BLUE}[*] Instalando tmux..${RESET}\n"
 rm -rf ~/.tmux
 git clone https://github.com/gpakosz/.tmux.git ~/.tmux
 ln -s -f ~/.tmux/.tmux.conf ~/
 cp -v $RPATH/CONFIGS/tmux.conf.local ~/.tmux.conf.local
 
-# nvim
-echo -e "\n${BLUE}[*] Instalando nvim..${RESET}\n"
-wget -q --show-progress https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz -O /tmp/nvim-linux64.tar.gz
-sudo tar xzvf /tmp/nvim-linux64.tar.gz --directory=/opt
-sudo ln -s /opt/nvim-linux64/bin/nvim /usr/bin/nvim
-sudo rm -f /opt/nvim-linux64.tar.gz
-
-# Instalar terminal kitty
 echo -e "\n${BLUE}[*] Instalando terminal kitty..${RESET}\n"
 cat $RPATH/kitty-installer.sh | sh /dev/stdin
 
-# Clonar repositorios de polybar & picom
 mkdir -p ~/github
 git clone --recursive https://github.com/polybar/polybar ~/github/polybar
 git clone https://github.com/ibhagwan/picom.git ~/github/picom
 
-# Instalar polybar
 echo -e "\n${BLUE}[*] Instalando polybar..${RESET}\n"
 cd ~/github/polybar
 mkdir build
@@ -143,14 +118,12 @@ cmake ..
 make -j$(nproc)
 sudo make install
 
-# Instalar temas de polybar
 echo -e "\n${BLUE}[*] Instalando temas de polybar..${RESET}\n"
 git clone --depth=1 https://github.com/adi1090x/polybar-themes.git ~/github/polybar-themes
 chmod +x ~/github/polybar-themes/setup.sh
 cd ~/github/polybar-themes
 echo 1 | ./setup.sh
 
-# Instalar picom
 echo -e "\n${BLUE}[*] Instalando picom..${RESET}\n"
 cd ~/github/picom
 git submodule update --init --recursive
@@ -158,13 +131,11 @@ meson --buildtype=release . build
 ninja -C build
 sudo ninja -C build install
 
-# Cambiar zona horaria
 echo -e "\n${BLUE}[*] Instalando configuraciones..${RESET}\n"
 sudo timedatectl set-timezone "Europe/Lisbon"
 
 mkdir -p ~/screenshots
 
-# Copiar configuraciones
 cp -rv $RPATH/CONFIGS/config/* ~/.config/
 cp -rv $RPATH/SCRIPTS/* ~/.config/polybar/forest/scripts/
 cp -v $RPATH/SCRIPTS/screenshot.sh ~/.config/polybar/obsidian/scripts/screenshot.sh
@@ -178,10 +149,12 @@ chmod +x ~/.config/bspwm/bspwmrc
 chmod +x ~/.config/bspwm/scripts/bspwm_resize
 chmod +x ~/.config/polybar/obsidian/launch.sh
 chmod +x ~/.config/polybar/obsidian/scripts/*.sh
+chmod +x ~/.config/polybar/obsidian-v2/launch.sh
+chmod +x ~/.config/polybar/obsidian-v2/scripts/*.sh
 chmod +x ~/.config/polybar/forest/scripts/target.sh
 chmod +x ~/.config/polybar/forest/scripts/screenshot.sh
 
-# Mensajes finales
 echo -e "\n${BLUE}[+] Entorno desplegado, Happy Hacking ;)${RESET}\n"
-echo -e "${BLUE}[+] Obsidian Polybar activo por defecto.${RESET}\n"
+echo -e "${BLUE}[+] Obsidian v2 activo por defecto.${RESET}\n"
+echo -e "${BLUE}[+] cat usa /usr/bin/cat y vim usa /usr/bin/vim.${RESET}\n"
 echo -e "${BLUE}[+] Por favor, reinicia el equipo (sudo reboot)${RESET}\n"
